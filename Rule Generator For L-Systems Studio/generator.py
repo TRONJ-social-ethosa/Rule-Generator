97d3347cf3a5f8617aeb9f9d678c5eec739a698f
tronj-np <58134602+tronj-np@users.noreply.github.com>
2020-05-26 16:35:24 +0300

    Add files via upload

--- Rule Generator For L-Systems Studio/generator.py
+++ Rule Generator For L-Systems Studio/generator.py
@@ -1,24 +1,55 @@
 from random import choice
 
 chars = list("+-[]{}<>fF|*/")
+axiom_chars = list("fFxyz+-")
+axiom = ''
 expression = ''
 
-count = input('Enter a number of chars: ')
+while True:
+    choose = input('[A]xiom or [R]ule: ')
 
-# generate expression
-if count.isdigit():
+    if choose.upper() == 'A':
+        count = input('Enter a number of chars: ')
 
-    for i in range(int(count)):
-        expression += choice(chars)
+        # axiom generator
+        if count.isdigit() and int(count) <= 50:
 
-print(expression)
-# save file with fractal
-choose = input('Save in file? [Y] or [N] ').upper()
-if choose == 'Y':
+            for i in range(int(count)):
+                if choice(axiom_chars) in '+-' and i == 1:
+                    pass
+                else:
+                    axiom += choice(axiom_chars)
 
-    path = input(r'Enter a path to save: ')
-    with open(path, 'a') as file:
-        file.write(expression)
+            # save file with axiom
+            choose2 = input('Save in file? [Y] or [N] ').upper()
+            if choose2 == 'Y':
 
-elif choose == 'N':
-    pass
+                path = input(r'Enter a path to save: ')
+                with open(path, 'w') as file:
+                    file.write(axiom)
+
+            elif choose2 == 'N':
+                pass
+        else:
+            print('Number of chars more than 50')
+            pass
+
+    elif choose.upper() == 'R':
+        count = input('Enter a number of chars: ')
+
+        # rule generator
+        if count.isdigit():
+
+            for i in range(int(count)):
+                expression += choice(chars)
+
+            # save file with fractal
+            choose2 = input('Save in file? [Y] or [N] ').upper()
+            if choose2 == 'Y':
+
+                path = input(r'Enter a path to save: ')
+                with open(path, 'w') as file:
+                    file.write(expression)
+
+            elif choose2 == 'N':
+                pass
