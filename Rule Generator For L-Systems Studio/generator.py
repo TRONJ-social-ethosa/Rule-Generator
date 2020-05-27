4581ef54af5c86fc4ae0897b3354cc6805ae10b2
red shark <58134602+tronj-np@users.noreply.github.com>
2020-05-27 19:58:53 +0300

    big update 0.2.2
    > impove the code
    > if/elif/else construction to if/elif/else + functions
    > code optimization
    > some medium bug fix
    > the code got a little faster
    > trello page update
    > added english trello page

--- Rule Generator For L-Systems Studio/generator.py
+++ Rule Generator For L-Systems Studio/generator.py
@@ -1,68 +1,104 @@
 from random import choice
 import re
 
-chars = list("+-[]{}<>fxfxfyfyFzFzF|*/")
-axiom_chars = list("fFxyz+-")
-axiom = ''
-expression = ''
+# functions
 
-while True:
-    choose = input('[A]xiom [R]ule: ')
+def axiomGenerate():
 
-    if choose.upper() == 'A':
-        count = input('Enter a number of chars: ')
+    # initialize variables
+    chars = list("+-[]{}<>fxfxfyfyFzFzF|*/")
+    axiom_chars = list("fFxyz+-")
+    axiom = ''
+    rule = ''
 
-        # axiom generator
-        if count.isdigit() and int(count) <= 50:
+    count = input('Enter a number of chars: ')
 
-            for i in range(int(count)):
-                random_char = choice(axiom_chars)
-                if random_char not in '+-' or i != 1:
-                    axiom += random_char
-            print(axiom)
-            # save file with axiom
-            path = input(r'Enter a path to save: ')
-            with open(path, 'w') as file:
-                file.write(axiom)
-        else:
-            print('Number of chars more than 50')
+    # axiom generator
+    if count.isdigit() and int(count) <= 50:
+        for i in range(int(count)):
+            random_char = choice(axiom_chars)
+            if random_char not in '+-' or i != 1:
+                axiom += random_char
 
-    elif choose.upper() == 'R':
-        axiom_rule = input('Rule for special axiom? [Y] or [N]: ')
-        if axiom_rule.upper() == 'Y':
-            chars2 = list("+-[]{}<>ffffFFFF|*/")
-            special_axiom = input('Enter axiom: ')
-            count = input('Enter a number of chars: ')
-            special_axiom_chars = re.findall(r"[\d\w]", special_axiom)
-            chars2 = chars2 + special_axiom_chars
-            axiom_rule_helper = ''
-            # rule generator
-            if count.isdigit():
+        # save file with axiom
+        path = input(r'Enter a path to save: ')
+        with open(path, 'w') as file:
+            file.write(axiom)
+    else:
+        print('Number of chars more than 50')
 
-                for i in range(int(count)):
-                    random_char = choice(chars2)
-                    if random_char not in '+-[]{}|*/' or i != 1:
-                        expression += random_char
-                print(expression)
-                for i in special_axiom_chars:
-                    axiom_rule_helper += str(i + "\n")
+def specialRuleGenerator():
 
-                # save file with special fractal
-                path = input(r'Enter a path to save: ')
-                with open(path, 'w') as file:
-                    file.write(axiom_rule_helper)
-        elif axiom_rule.upper() == 'N':
-            count = input('Enter a number of chars: ')
+    # initialize variables
+    chars = list("+-[]{}<>fxfxfyfyFzFzF|*/")
+    axiom_chars = list("fFxyz+-")
+    axiom = ''
+    rule = ''
+    chars2 = list("+-[]{}<>ffffFFFF|*/")
+    special_axiom = input('Enter axiom: ')
+    count = input('Enter a number of chars: ')
 
-            # rule generator
-            if count.isdigit():
+    # create new chars list
+    special_axiom_chars = re.findall(r"[\d\w]", special_axiom)
+    chars2 = chars2 + special_axiom_chars
+    axiom_rule_helper = ''
 
-                for i in range(int(count)):
-                    random_char = choice(chars)
-                    if random_char not in '+-[]{}|*/' or i != 1:
-                        expression += random_char
+    # special rule generator
+    if count.isdigit():
+        for i in range(int(count)):
+            random_char = choice(chars2)
+            if random_char not in '+-[]{}|*/' or i != 1:
+                rule += random_char
 
-                # save file with fractal
-                path = input(r'Enter a path to save: ')
-                with open(path, 'w') as file:
-                    file.write(expression)
+        for i in special_axiom_chars:
+            axiom_rule_helper += str(i + "\n")
+
+        # save file with special fractal
+        path = input(r'Enter a special fractal path to save: ')
+        with open(path, 'w') as file:
+            file.write(rule)
+
+        # save file with axiom helper
+        path = input(r'Enter a axiom helper path to save: ')
+        with open(path, 'w') as file:
+            file.write(axiom_rule_helper)
+
+def ruleGenerator():
+    # initialize variables
+    chars = list("+-[]{}<>fxfxfyfyFzFzF|*/")
+    axiom_chars = list("fFxyz+-")
+    axiom = ''
+    rule = ''
+    count = input('Enter a number of chars: ')
+
+    # rule generator
+    if count.isdigit():
+        for i in range(int(count)):
+            random_char = choice(chars)
+            if random_char not in '+-[]{}|*/' or i != 1:
+                rule += random_char
+
+        # save file with fractal
+        path = input(r'Enter a path to save: ')
+        with open(path, 'w') as file:
+            file.write(rule)
+
+# main cycle
+def main():
+    while True:
+        choose = input('[A]xiom [R]ule: ')
+
+        if choose.upper() == 'A':
+                axiomGenerate()
+
+        elif choose.upper() == 'R':
+            axiom_rule = input('Rule for special axiom? [Y] or [N]: ')
+
+            if axiom_rule.upper() == 'Y':
+                specialRuleGenerator()
+
+            elif axiom_rule.upper() == 'N':
+                ruleGenerator()
+
+if __name__ == '__main__':
+    main()
