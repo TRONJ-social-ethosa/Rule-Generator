e006d5b676f8dc03c55f67dcfed625636844da64
tronj-np <58134602+tronj-np@users.noreply.github.com>
2020-05-27 09:24:25 +0300

    big update
    > bug fix
    > added rule generator for a user axiom
    > code optimization
    > trello info update

--- Rule Generator For L-Systems Studio/generator.py
+++ Rule Generator For L-Systems Studio/generator.py
@@ -1,4 +1,5 @@
 from random import choice
+import re
 
 chars = list("+-[]{}<>fxfxfyfyFzFzF|*/")
 axiom_chars = list("fFxyz+-")
@@ -6,7 +7,7 @@ axiom = ''
 expression = ''
 
 while True:
-    choose = input('[A]xiom or [R]ule: ')
+    choose = input('[A]xiom [R]ule: ')
 
     if choose.upper() == 'A':
         count = input('Enter a number of chars: ')
@@ -19,7 +20,7 @@ while True:
                     pass
                 else:
                     axiom += choice(axiom_chars)
-
+            print(axiom)
             # save file with axiom
             path = input(r'Enter a path to save: ')
             with open(path, 'w') as file:
@@ -30,19 +31,45 @@ while True:
             pass
 
     elif choose.upper() == 'R':
-        count = input('Enter a number of chars: ')
+        axiom_rule = input('Rule for special axiom? [Y] or [N]: ')
+        if axiom_rule.upper() == 'Y':
+            chars2 = list("+-[]{}<>ffffFFFF|*/")
+            special_axiom = input('Enter axiom: ')
+            count = input('Enter a number of chars: ')
+            special_axiom_chars = re.findall(r"[\d\w]", special_axiom)
+            chars2 = chars2 + special_axiom_chars
+            axiom_rule_helper = ''
+            # rule generator
+            if count.isdigit():
 
-        # rule generator
-        if count.isdigit():
+                for i in range(int(count)):
+                    if choice(chars2) in list('+-[]{}|*/') and i == 1:
+                        pass
+                    else:
+                        expression += choice(chars2)
+                print(expression)
+                for i in special_axiom_chars:
+                    axiom_rule_helper += str(i + "\n")
 
-            for i in range(int(count)):
-                if choice(chars) in list('+-[]{}|*/') and i == 1:
-                    pass
-                else:
-                    expression += choice(chars)
+                # save file with special fractal
+                path = input(r'Enter a path to save: ')
+                with open(path, 'w') as file:
+                    file.write(axiom_rule_helper)
+                    file.close()
+        elif axiom_rule.upper() == 'N':
+            count = input('Enter a number of chars: ')
 
-            # save file with fractal
-            path = input(r'Enter a path to save: ')
-            with open(path, 'w') as file:
-                file.write(expression)
-                file.close()
+            # rule generator
+            if count.isdigit():
+
+                for i in range(int(count)):
+                    if choice(chars) in list('+-[]{}|*/') and i == 1:
+                        pass
+                    else:
+                        expression += choice(chars)
+
+                # save file with fractal
+                path = input(r'Enter a path to save: ')
+                with open(path, 'w') as file:
+                    file.write(expression)
+                    file.close()
